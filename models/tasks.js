e82c86fd8b22c575504da6c3fdeba172ea8d8ade
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2019-01-02 13:52:43 +0400

    added model of sequelize

--- models/tasks.js
+++ models/tasks.js
@@ -1,10 +1,32 @@
-module.exports = app => {
-	return {
-		findAll: (param, callback) => {
-			return callback([
-				{title: "Buy Some shoes"},
-				{title: "Fix Notebook"},
-			]);
+module.exports = (sequelize, DataType) => {
+	const Tasks = sequelize.define("Tasks",
+		{
+			id: {
+				type: DataType.INTEGER,
+				primaryKey: true,
+				autoIncrement: true
+			},
+			title: {
+				type: DataType.String,
+				allowNull: false,
+				validate: {
+					notEmpty: true
+				}
+			},
+			done: {
+				type: DataType.Boolean,
+				allowNull: false,
+				defaultValue: false
+			}
+		},
+		{
+			classMethods: {
+				associate: (models) => {
+					Tasks.belongsTo(models.Users);
+				}
+			}
 		}
-	};
+	);
+
+	return Tasks;
 };
