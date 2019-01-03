b69d14b898bf3fa17e7052970422292e0578cb9f
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2019-01-03 19:48:00 +0400

    added tasks main routes

--- routes/tasks.js
+++ routes/tasks.js
@@ -7,12 +7,18 @@ module.exports = app => {
 		  // middleware for preexecution of routes
 	  })
 		.get("/tasks", (req, res) => {
-	    Tasks.findAll({}).then(tasks => {
-	      res.json({tasks: tasks});
-	    });
-	  })
+	    Tasks.findAll({})
+		    .then(result => res.json(result))
+		    .catch(error => {
+		    	res.status(412).json({msg: error.message});
+		    })
+		})
 		.post((req, res) => {
-
+			Tasks.create(req.body)
+				.then(result => res.json(result))
+				.catch(error => {
+					res.status(412).json({msg: error.message})
+				})
 	  });
 
 	app.route("/tasks/:id")
