3e6e7217ad066e60b0dc17c384627b14516daed9
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2019-01-03 19:41:31 +0400

    added routes, and middlewares

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,33 @@
 module.exports = app => {
   const Tasks = app.db.models.Tasks;
-  app.get("/tasks", (req, res) => {
-    Tasks.findAll({}).then(tasks => {
-      res.json({tasks: tasks});
-    });
-  });
+	app.route("/tasks")
+		.all((req, res) => {
+			delete req.body.id;
+			next();
+		  // middleware for preexecution of routes
+	  })
+		.get("/tasks", (req, res) => {
+	    Tasks.findAll({}).then(tasks => {
+	      res.json({tasks: tasks});
+	    });
+	  })
+		.post((req, res) => {
+
+	  });
+
+	app.route("/tasks/:id")
+		.all((req, res) => {
+			delete req.body.id;
+			next();
+			// MIddleware for execution of routes
+		})
+		.get((req, res) => {
+			// /tasks/1 : find a task
+		})
+		.put((req, res) => {
+			// /tasks/1
+		})
+		.delete((req, res) => {
+			// /tasks/1 : delete a task
+		});
 };
