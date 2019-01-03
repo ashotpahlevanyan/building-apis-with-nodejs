2bc8a945a4b49ccba6a6b91df83649366b064a11
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2019-01-03 19:57:28 +0400

    added crud to tasks/:id

--- routes/tasks.js
+++ routes/tasks.js
@@ -4,36 +4,52 @@ module.exports = app => {
 		.all((req, res) => {
 			delete req.body.id;
 			next();
-		  // middleware for preexecution of routes
 	  })
 		.get("/tasks", (req, res) => {
 	    Tasks.findAll({})
 		    .then(result => res.json(result))
 		    .catch(error => {
 		    	res.status(412).json({msg: error.message});
-		    })
+		    });
 		})
 		.post((req, res) => {
 			Tasks.create(req.body)
 				.then(result => res.json(result))
 				.catch(error => {
 					res.status(412).json({msg: error.message})
-				})
+				});
 	  });
 
 	app.route("/tasks/:id")
 		.all((req, res) => {
 			delete req.body.id;
 			next();
-			// MIddleware for execution of routes
 		})
 		.get((req, res) => {
-			// /tasks/1 : find a task
+			Tasks.findOne({where: req.params})
+				.then(result => {
+					if(result) {
+						res.json(result);
+					} else {
+						res.sendStatus(404);
+					}
+				})
+				.catch(error => {
+					res.status(412).json({msg: error.message});
+				});
 		})
 		.put((req, res) => {
-			// /tasks/1
+			Tasks.update(req.body, {where: req.params})
+				.then(result => res.sendStatus(204))
+				.catch(error => {
+					res.status(412).json({msg: error.message});
+				});
 		})
 		.delete((req, res) => {
-			// /tasks/1 : delete a task
+			Tasks.destroy({where: req.params})
+				.then(result => res.sendStatus(204))
+				.catch(error => {
+					res.status(412).json({msg: error.message})
+				});
 		});
 };
