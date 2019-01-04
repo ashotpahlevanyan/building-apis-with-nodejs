f88d2943c1938a172801b6e9dde10074c57ad83e
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2019-01-04 12:55:39 +0400

    added test and development configs

---
+++ libs/config.test.js
@@ -0,0 +1,15 @@
+module.exports = {
+	database: "ntask",
+	username: "",
+	password: "",
+	params: {
+		dialect: "sqlite",
+		storage: "ntask.sqlite",
+		logging: "false",
+		define: {
+			underscored: true
+		}
+	},
+	jwtSecret: "Nta$K-AP1",
+	jwtSession: {session: false}
+};
